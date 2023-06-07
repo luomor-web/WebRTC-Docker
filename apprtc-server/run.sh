2609211df10e0a638c7da5d5e7cef6c145ef73d6
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-06-08 00:09:33 +0800

    update

--- apprtc-server/run.sh
+++ apprtc-server/run.sh
@@ -1,5 +1,5 @@
 sed -i "s/SERVER_PUBLIC_IP/${PUBLIC_IP}/g" /ice.js
-sed -i 's/wss:\/\//ws:\/\//g' /apprtc/out/app_engine/apprtc.py
+#sed -i 's/wss:\/\//ws:\/\//g' /apprtc/out/app_engine/apprtc.py
 sed -i 's/https:\/\//http:\/\//g' /apprtc/out/app_engine/apprtc.py
 
 #sed -i "s/SERVER_PUBLIC_IP/${PUBLIC_IP}/g" /apprtc/out/app_engine/constants.py
